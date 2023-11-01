a805a0da8d10e31ea2951a8225461bdb546ff3c4
HyanNano <kengnihyan5@gmail.com>
2023-11-01 16:40:01 +0100

    new real one

--- MedecineAppProject/MedecineAppProject/asgi.py
+++ MedecineAppProject/MedecineAppProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MedecineApp project.
+ASGI config for MedecineAppProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedecineApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedecineAppProject.settings')
 
 application = get_asgi_application()
--- MedecineAppProject/MedecineAppProject/urls.py
+++ MedecineAppProject/MedecineAppProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MedecineApp project.
+URL configuration for MedecineAppProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
